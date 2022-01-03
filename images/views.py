14d8e88612279af3014c3656531efd9231f2bf5d
rohamm <rohambadi547@gmail.com>
2022-01-03 11:17:45 -0800

    action app for notification or new events.

--- images/views.py
+++ images/views.py
@@ -10,6 +10,7 @@ from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 from common.decorators import ajax_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from actions.utils import create_action
 
 
 @login_required
@@ -21,6 +22,7 @@ def image_post(request):
             new_item = form.save(commit=False)
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'posted image', new_item)
             messages.success(request, 'Image add successfully!')
             return redirect(new_item.get_absolute_url())
             # return redirect('images:compost')
@@ -68,6 +70,7 @@ def image_create(request):
             new_item = form.save(commit=False)
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image add successfully!')
             return redirect(new_item.get_absolute_url())
 
@@ -94,6 +97,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
